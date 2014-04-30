e34e623d6097534c6b197b31c5f217b6f9053957
mmendoza2 <man.m25@gmail.com>
2014-04-30 17:28:18 -0500

    Agregando campos a usuario admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429204808) do
+ActiveRecord::Schema.define(version: 20140430215620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -370,6 +370,7 @@ ActiveRecord::Schema.define(version: 20140429204808) do
     t.text     "lat"
     t.text     "lng"
     t.boolean  "editor"
+    t.string   "editor_estado"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
