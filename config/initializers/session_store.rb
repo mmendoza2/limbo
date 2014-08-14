adf6149d8a7b718b501af1f86f61e1a8ebfa7c45
Manuel Mendoza <man.m25@gmail.com>
2014-08-14 10:57:42 -0500

    Cambiando nombre titulo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Limbo::Application.config.session_store :cookie_store, key: '_notelimites_session'
+Limbo::Application.config.session_store :cookie_store, key: '_limbo_session'
